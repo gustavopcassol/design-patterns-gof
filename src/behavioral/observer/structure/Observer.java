98ee11a2e7f59da306ec9e7b355af16de0e7a783
gustavo <gustavopcassol@gmail.com>
2021-01-21 01:30:14 -0300

    added empty structure classes

---
+++ src/behavioral/observer/structure/Observer.java
@@ -0,0 +1,4 @@
+package behavioral.observer.structure;
+
+class Observer {
+}
---
+++ src/behavioral/observer/structure/Subject.java
@@ -0,0 +1,5 @@
+package behavioral.observer.structure;
+
+class Subject {
+    // todo implement this and others in package.
+}
