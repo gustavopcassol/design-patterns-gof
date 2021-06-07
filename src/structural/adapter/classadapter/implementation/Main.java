5dc54333cfe56983a9bc9967af14c6ea357d8133
Gustavo Krieger <gustavopcassol@gmail.com>
2021-06-06 22:33:28 -0300

    rename variables in adapter

--- src/structural/adapter/classadapter/implementation/Main.java
+++ src/structural/adapter/classadapter/implementation/Main.java
@@ -5,8 +5,8 @@ import structural.adapter.classadapter.implementation.participants.Target;
 
 class Main {
     public static void main(String[] args) {
-        Target target = new Adapter();
-        Iterable<Character> characters = target.request();
+        Target adapter = new Adapter();
+        Iterable<Character> characters = adapter.request();
         System.out.println(characters);
     }
 }
--- src/structural/adapter/objectadapter/implementation/Main.java
+++ src/structural/adapter/objectadapter/implementation/Main.java
@@ -7,8 +7,8 @@ import structural.adapter.objectadapter.implementation.participants.Target;
 class Main {
     public static void main(String[] args) {
         Adaptee adaptee = new Adaptee();
-        Target target = new Adapter(adaptee);
-        Iterable<Character> characters = target.request();
+        Target adapter = new Adapter(adaptee);
+        Iterable<Character> characters = adapter.request();
         System.out.println(characters);
     }
 }
